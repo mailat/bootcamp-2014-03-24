92f30b9f89dfeacfa34038f72c4a09c0dbc47626
Marius Mailat <marius.mailat@appseleration.com>
2014-03-25 12:27:28 +0400

    + preference screen

--- Samples/Yamba/src/com/intel/yamba/StatusActivity.java
+++ Samples/Yamba/src/com/intel/yamba/StatusActivity.java
@@ -3,22 +3,36 @@ package com.intel.yamba;
 import com.marakana.android.yamba.clientlib.YambaClient;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
-public class StatusActivity extends Activity {
+public class StatusActivity extends Activity implements OnSharedPreferenceChangeListener{
 
+	private String username = null;
+	private String password = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_status);
+		
+		//get the initial values from preferences
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);		
+		username = prefs.getString("username", null);
+		password = prefs.getString("password", null);	
+		
+		//register for the preference changes
+		prefs.registerOnSharedPreferenceChangeListener(this);
 	}
 
 	@Override
@@ -35,6 +49,7 @@ public class StatusActivity extends Activity {
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
+			startActivity(new Intent(this, PrefsActivity.class));
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
@@ -47,8 +62,14 @@ public class StatusActivity extends Activity {
 		// we got reference to the EditText and we read the status
 		EditText editText = (EditText) findViewById(R.id.editText);
 
-		//call the AsyncTask poster to send the text	
-		new PostToTwitter().execute(editText.getText().toString());
+		//check if we have a username, password; if not redirect to preferences
+		if (username!=null && password!=null)
+		{
+			//call the AsyncTask poster to send the text
+			new PostToTwitter().execute(editText.getText().toString());
+		}
+		else
+			startActivity(new Intent(this, PrefsActivity.class));
 	}
 
 	class PostToTwitter extends AsyncTask<String, Integer, String> {
@@ -59,7 +80,7 @@ public class StatusActivity extends Activity {
 
 			try {
 				// post on Twitter
-				YambaClient client = new YambaClient("marius", "password");
+				YambaClient client = new YambaClient(username, password);
 				client.postStatus(statuses[0]);
 				return "Posted ok the text" + statuses[0];
 			} catch (Throwable e) {
@@ -80,6 +101,14 @@ public class StatusActivity extends Activity {
 			progress = ProgressDialog.show(StatusActivity.this, "Posting to yamba server", "Please wait ....");
 		}
 
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,String key) {
+		if (key.equals("username"))
+			username = sharedPreferences.getString(key, null);
+		else if (key.equals("password"))
+			password = sharedPreferences.getString(key, null);
 	}
 
 }
